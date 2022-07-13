373fed91d3dd217d48386e265d468593b8fcb8a7
Philip Mattsson <maini.ac@hotmail.com>
2022-07-13 20:48:41 +0200

    Added ConnectionString

--- BulkyBookWeb/Models/Category.cs
+++ BulkyBookWeb/Models/Category.cs
@@ -1,6 +1,15 @@
-﻿namespace BulkyBookWeb.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BulkyBookWeb.Models
 {
-    public class Categorycs
+    public class Category
     {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+        public int DispalyOrder { get; set; }
+        public DateTime CreatedDateTime { get; set; } = DateTime.Now;
     }
 }
