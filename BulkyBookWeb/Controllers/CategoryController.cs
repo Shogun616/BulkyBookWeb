eec5bcfd107036d9b3ade746a890b68b1d48c221
Philip Mattsson <maini.ac@hotmail.com>
2022-07-19 19:49:09 +0200

    Added TempData

--- BulkyBookWeb/Controllers/CategoryController.cs
+++ BulkyBookWeb/Controllers/CategoryController.cs
@@ -36,6 +36,7 @@ namespace BulkyBookWeb.Controllers
             {
                 _db.Categories.Add(obj);
                 _db.SaveChanges();
+                TempData["success"] = "Category created successfully";
                 return RedirectToAction("Index");
             }
             return View(obj);
@@ -72,6 +73,7 @@ namespace BulkyBookWeb.Controllers
             {
                 _db.Categories.Update(obj);
                 _db.SaveChanges();
+                TempData["success"] = "Category updated successfully";
                 return RedirectToAction("Index");
             }
             return View(obj);
@@ -107,6 +109,7 @@ namespace BulkyBookWeb.Controllers
             
             _db.Categories.Remove(obj);
             _db.SaveChanges();
+            TempData["success"] = "Category deleted successfully";
             return RedirectToAction("Index");
         }
     }
